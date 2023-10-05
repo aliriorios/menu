f12ab87c999ab2da6730fa4e7792cb03a2a6e3e5
Alírio Rios <contato.aliriorios@gmail.com>
2023-10-05 14:23:36 -0300

    Implementando os valores da compra

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@ $(document).ready(function () {
     cardapio.eventos.init();
 })
 
+// Objeto que guarda eventos, métodos e templates
+// Uma ideia de como aplicar POO
 var cardapio = {};
 
 var MEU_CARRINHO = [];
+
+var VALOR_CARRINHO = 0;
+var VALOR_ENTREGA = 5;
 
 cardapio.eventos = {
     init: () => {
@@ -212,10 +217,16 @@ cardapio.metodos = {
                 .replace(/\${qntd}/g, e.qntd);
                 
                 $("#itensCarrinho").append(temp);
+
+                // Último item do carrinho
+                if ((i + 1) == MEU_CARRINHO.length) {
+                    cardapio.metodos.carregarValores();
+                }
             })
 
         } else {
             $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i>Seu carrinho está vazio.</p>');
+            cardapio.metodos.carregarValores();
         }
     },
 
@@ -252,6 +263,28 @@ cardapio.metodos = {
 
         // Atualiza o badge do botão carrinho
         cardapio.metodos.atualizarBadgeTotal();
+
+        // Atualiza os valores totais do carrinho
+        cardapio.metodos.carregarValores();
+    },
+
+    carregarValores: () => {
+        VALOR_CARRINHO = 0;
+
+        // Zerando os valores
+        $("#lblSubTotal").text('R$ 0,00');
+        $("#lblValorEntrega").text('+ R$ 0,00');
+        $("#lblValorTotal").text('R$ 0,00');
+
+        $.each(MEU_CARRINHO, (i, e) => {
+            VALOR_CARRINHO += parseFloat(e.price * e.qntd);
+
+            if ((i + 1) == MEU_CARRINHO.length) {
+                $("#lblSubTotal").text(`R$ ${VALOR_CARRINHO.toFixed(2).replace(".", ",")}`);
+                $("#lblValorEntrega").text(`+ R$ ${VALOR_ENTREGA.toFixed(2).replace(".", ",")}`);
+                $("#lblValorTotal").text(`R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace(".", ",")}`);
+            }
+        })
     },
 
     mensagem: (texto, cor = 'red', tempo = 3000) => {

158a67c75a84c0b932f99d315a92caff64f277aa
aliriorios <alirioriosmc@gmail.com>
2024-01-08 13:27:04 -0300

    Responsividade no cardapio

--- js/app.js
+++ js/app.js
@@ -517,18 +517,19 @@ cardapio.metodos = {
 // TEMPLATES ----------------------------------------------------------------
 cardapio.templates = {
     item: `
-        <div class="col-3 mb-5 wow fadeInUp">
+        <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 animated fadeInUp">
             <div class="card card-item" id="\${id}">
                 <div class="img-produto">
-                <img src="\${img}" alt="">
-                    <p class="title-produto text-center mt-4"><strong>\${name}</strong></p>
-                    <p class="price-produto text-center"><strong>R$ \${price}</strong></p>
-                    <div class="add-carrinho">
-                        <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
-                        <span class="add-numero-itens" id="qntd-\${id}">0</span>
-                        <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
-                        <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fa fa-shopping-bag"></i></span>
-                    </div>
+                    <img src="\${img}" alt="">
+                </div>
+
+                <p class="title-produto text-center mt-4"><strong>\${name}</strong></p>
+                <p class="price-produto text-center"><strong>R$ \${price}</strong></p>
+                <div class="add-carrinho">
+                    <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+                    <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                    <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
+                    <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fa fa-shopping-bag"></i></span>
                 </div>
             </div>
         </div>
